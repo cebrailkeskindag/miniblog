cff12de5f3bc745fb7e146d37d75ee6c2cf18f44
cebrailkeskindag <cebrailkeskindag1549@gmail.com>
2023-12-27 19:50:04 +0300

    .

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,65 +3,71 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:miniblog/blocs/article_bloc/article_bloc.dart';
 import 'package:miniblog/blocs/article_bloc/article_event.dart';
 import 'package:miniblog/blocs/article_bloc/article_state.dart';
-import 'package:miniblog/models/blog.dart';
-import 'package:miniblog/repositories/article_repository.dart';
 import 'package:miniblog/screens/add_blog.dart';
 import 'package:miniblog/widgets/blog_item.dart';
 
 class Homepage extends StatefulWidget {
-  const Homepage({Key? key}) : super(key: key);
+  const Homepage({super.key});
 
   @override
-  _HomepageState createState() => _HomepageState();
+  State<Homepage> createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
-  List<Blog> blogs = [];
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Bloglar"),
-          actions: [
-            IconButton(
+      appBar: AppBar(
+        title: const Text("Bloglar"),
+        actions: [
+          IconButton(
               onPressed: () async {
                 bool? result = await Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (ctx) => const AddBlog()));
+                    // ignore: prefer_const_constructors
+                    .push(MaterialPageRoute(builder: (ctx) => AddBlog()));
+
                 if (result == true) {
                   context.read<ArticleBloc>().add(FetchArticles());
                 }
               },
-              icon: const Icon(Icons.add),
-            )
-          ],
-        ),
-        body: BlocProvider(
-            create: (context) =>
-                ArticleBloc(articleRepository: ArticleRepository()),
-            child: BlocBuilder<ArticleBloc, ArticleState>(
-                builder: (context, state) {
-              if (state is ArticlesInitial) {
-                //bloc'a fetchArticles eventi göndermek
-                context
-                    .read<ArticleBloc>()
-                    .add(FetchArticles()); //UI'dan BLOC'a Event
-                return const Center(child: Text("İstek atılıyor.."));
-              }
-              if (state is ArticlesLoading) {
-                return const Center(child: CircularProgressIndicator());
-              }
-              if (state is ArticlesLoaded) {
-                return ListView.builder(
-                    itemCount: state.blogs.length,
-                    itemBuilder: (context, index) =>
-                        BlogItem(blog: state.blogs[index]));
-              }
-              if (state is ArticlesError) {
-                return const Center(
-                    child: Text("Bloglar yüklenirken bir hata oluştu."));
-              }
-              return const Center(child: Text("Unknown State"));
-            })));
+              icon: const Icon(Icons.add))
+        ],
+      ),
+      body: BlocBuilder<ArticleBloc, ArticleState>(builder: (context, state) {
+        if (state is ArticlesInitial) {
+          // bloc'a fetcharticles eventi göndermek
+          context
+              .read<ArticleBloc>()
+              .add(FetchArticles()); // UI'dan BLOC'a Event
+          return const Center(child: Text("İstek atılıyor..."));
+        }
+
+        if (state is ArticlesLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (state is ArticlesLoaded) {
+          return ListView.builder(
+              itemCount: state.blogs.length,
+              itemBuilder: (context, index) =>
+                  BlogItem(blog: state.blogs[index]));
+        }
+
+        if (state is ArticlesError) {
+          return const Center(
+            child: Text("Bloglar yüklenirken bir hata oluştu."),
+          );
+        }
+
+        return const Center(
+          child: Text("Unknown State"),
+        );
+      }),
+    );
   }
 }
